57518cbabaa960422c80d90bbd892070502cbf85
kyeongseo <keongseo1234@gmail.com>
2023-10-04 23:49:46 +0900

    change ide setting from m2

---
+++ src/main/java/com/example/chat_board/ChatBoardApplication.java
@@ -0,0 +1,13 @@
+package com.example.chat_board;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChatBoardApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ChatBoardApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/chat_board/ConnectionTest.java
@@ -0,0 +1,29 @@
+package com.example.chat_board;
+
+import org.hibernate.boot.Metadata;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import javax.sql.DataSource;
+import java.sql.Connection;
+import java.sql.DatabaseMetaData;
+import java.sql.SQLException;
+
+@SpringBootTest
+public class ConnectionTest {
+
+    @Autowired
+    DataSource dataSource;
+
+    @Test
+    public void test() throws SQLException {
+        Connection con = dataSource.getConnection();
+        DatabaseMetaData meta = con.getMetaData();
+        System.out.println("user name:" + meta.getUserName());
+        System.out.println("sql driver:" + meta.getDriverName());
+        System.out.println("sql url: " + meta.getURL());
+    }
+
+
+}
