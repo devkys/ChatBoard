0a05fb9b82ebd30c9bb2694ef065fec334e2ceeb
kyeongseo <keongseo1234@gmail.com>
2023-10-05 23:57:13 +0900

    mssql jdbc connection test code

--- src/test/java/com/example/chat_board/ConnectionTest.java
+++ src/test/java/com/example/chat_board/ConnectionTest.java
@@ -1,6 +1,5 @@
 package com.example.chat_board;
 
-import org.hibernate.boot.Metadata;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
