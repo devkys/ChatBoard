d29adc332df4e27632fcfdc79bda0a5c2ea6e072
kyeongseo <keongseo1234@gmail.com>
2023-10-07 15:03:03 +0900

    not black member domain

--- src/main/java/com/example/chat_board/member/Member.java
+++ src/main/java/com/example/chat_board/member/Member.java
@@ -3,9 +3,12 @@ package com.example.chat_board.member;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
+import jakarta.validation.constraints.NotBlank;
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import org.antlr.v4.runtime.misc.NotNull;
 
 @Getter
 @NoArgsConstructor
@@ -15,12 +18,22 @@ import lombok.NoArgsConstructor;
 public class Member {
 
     @Id
-    private String user_id;
-    private String user_name;
-    private String pw;
-    private String addr;
-    private String phone;
-    private String email;
+    @NotBlank
+    private String user_id; // 필수 매개변수
 
+    @NotBlank
+    private String pw; // 필수 매개변수
 
+    @NotBlank
+    private String user_name; // 선택적 매개변수
+    private String addr; // 선택적 매개변수
+    private String phone; // 선택적 매개변수
+    private String email; // 선택적 매개변수
+
+    @Builder
+    public Member(String user_id, String pw, String user_name){
+       this.user_id = user_id;
+       this.pw = pw;
+       this.user_name = user_name;
+    }
 }
