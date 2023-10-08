ecf30dc9e0e607bd832861b3bd3b471a7147364a
kyeongseo <keongseo1234@gmail.com>
2023-10-08 22:26:23 +0900

    add dto class

---
+++ src/main/java/com/example/chat_board/member/MemberDTO.java
@@ -0,0 +1,14 @@
+package com.example.chat_board.member;
+
+import lombok.Getter;
+
+@Getter
+public class MemberDTO {
+
+    private String user_id;
+    private String pw;
+    private String user_name;
+    private String addr;
+    private String phone;
+    private String email;
+}
