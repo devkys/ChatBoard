b5a427f8f90cf564b0126aa6a61fc0bf4d8065de
kyeongseo <keongseo1234@gmail.com>
2023-10-09 02:34:06 +0900

    private final memberservice & signup method controller

--- src/main/java/com/example/chat_board/member/MemberController.java
+++ src/main/java/com/example/chat_board/member/MemberController.java
@@ -1,4 +1,30 @@
 package com.example.chat_board.member;
 
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("member/")
+@RequiredArgsConstructor
 public class MemberController {
+
+    private final MemberService memberService;
+
+    @GetMapping("signup")
+    public String signup(MemberDTO memberDTO) {
+        String code = "";
+        int result = memberService.signup(memberDTO);
+        if (result == 1) {
+            code = "회원 가입 성공";
+        } else {
+            code = "해당 아이디 이미 사용 중";
+        }
+        return code;
+    }
+
+
 }
+
+
