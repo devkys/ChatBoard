6e1057d2d9fe51ea43662ef8644b7ced46078f6e
kyeongseo <keongseo1234@gmail.com>
2023-10-13 23:34:32 +0900

    return dto to entity method

--- src/main/java/com/example/chat_board/member/SignupDto.java
+++ src/main/java/com/example/chat_board/member/SignupDto.java
@@ -1,5 +1,6 @@
 package com.example.chat_board.member;
 
+import jakarta.validation.constraints.NotBlank;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -9,11 +10,27 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 public class SignupDto {
 
+    @NotBlank
     private String user_id;
+    @NotBlank
     private String pw;
+    @NotBlank
     private String pw_confirm;
+    @NotBlank
     private String user_name;
     private String addr;
     private String phone;
     private String email;
+
+    public Member toEntity(){
+        return Member.builder()
+                .user_id(user_id)
+                .pw(pw)
+                .user_name(user_name)
+                .addr(addr)
+                .phone(phone)
+                .email(email)
+                .build();
+    }
+
 }
