00592bcfd99108d2c609dee7a9f1cb4b4c5b7f3a
kyeongseo <keongseo1234@gmail.com>
2023-10-17 23:56:38 +0900

    add findbyemail method

--- src/main/java/com/example/chat_board/member/MemberRepository.java
+++ src/main/java/com/example/chat_board/member/MemberRepository.java
@@ -3,6 +3,7 @@ package com.example.chat_board.member;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface MemberRepository extends JpaRepository<Member, String> {
-
+    // 이메일 중복확인
+    Member findByEmail(String email);
 
 }
