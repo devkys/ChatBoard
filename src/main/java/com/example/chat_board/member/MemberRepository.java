65795da9b433b57d8baef53fa78475418ceed95f
kyeongseo <keongseo1234@gmail.com>
2023-10-18 23:49:45 +0900

    findbyemail

--- src/main/java/com/example/chat_board/member/MemberRepository.java
+++ src/main/java/com/example/chat_board/member/MemberRepository.java
@@ -4,6 +4,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface MemberRepository extends JpaRepository<Member, String> {
     // 이메일 중복확인
-    Member findByEmail(String email);
+    Member findByEmail (String email);
 
 }
