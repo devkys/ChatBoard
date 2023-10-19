31f8bd323a1719eed519bda64be20f40c63699aa
kyeongseo <keongseo1234@gmail.com>
2023-10-19 23:58:24 +0900

    session login

--- src/main/java/com/example/chat_board/member/MemberController.java
+++ src/main/java/com/example/chat_board/member/MemberController.java
@@ -4,7 +4,6 @@ import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpSession;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
-import org.springframework.http.HttpRequest;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
@@ -18,13 +17,19 @@ public class MemberController {
 
     private final MemberService memberService;
 
+    /*
+     * 회원 가입 getMapping
+     */
     @GetMapping("signup")
     public String signup(Model model) {
         model.addAttribute("signupDto", new SignupDto());
 
         return "member/signup_form";
     }
-    // 회원가입
+
+    /*
+     * 회원 가입 postMapping
+     */
     @PostMapping("signup")
     public String signup(@Valid SignupDto signupDto, BindingResult bindingResult, Model model) {
         if(bindingResult.hasErrors()) {
@@ -34,14 +39,29 @@ public class MemberController {
 
             return "member/signup_form";
         }
+
+        // 이메일 중복 확인
+        int idDuplicate = memberService.idCheck(signupDto.getEmail());
+        if(idDuplicate == 0) {
+            bindingResult.rejectValue("email", "emailDuplicated","존재하는 이메일");
+            return "member/signup_form";
+
+        }
+
+        // 패스워드 확인
         if(!signupDto.getPw_confirm().equals(signupDto.getPw())) {
             bindingResult.rejectValue("pw_confirm", "passwordIncorrect", "2개의 패스워드 불일치");
             return "member/signup_form";
         }
+
+        // member signup
         memberService.signup(signupDto);
         return "member/board";
     }
 
+    /*
+     * 로그인 getMapping
+     */
     @GetMapping("signin")
     public String sign_in(Model model) {
         model.addAttribute("signinDto", new SigninDto());
@@ -49,20 +69,25 @@ public class MemberController {
         return "member/signin";
     }
 
+    /*
+     * 로그인 postMapping
+     */
     @PostMapping("signin")
-    public String signin(@Valid  SigninDto signinDto, Model model, HttpServletRequest request) {
+    public String signin(@Valid SigninDto signinDto, Model model, HttpServletRequest request) {
         Member memberLog = memberService.signin(signinDto);
         if(memberLog != null) {
             HttpSession session = request.getSession();
-            session.setAttribute("member", memberLog);
-
+            session.setAttribute("member_info", memberLog);
         }
-
         else {
             return "member/signin";
         }
         return "member/board";
     }
+
+    /*
+     * 로그아웃
+     */
     @PostMapping("/logout")
     public String logout(HttpSession session) {
         session.invalidate();
